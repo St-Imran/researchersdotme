5f6411b547f9e968909fabd238a25668ae4c986b
Afaque-zia <afaquezianizami@gmail.com>
2024-04-27 22:31:34 +0530

    added footer header, footer top, and side fixed contact us button

---
+++ src/app/Common/Contact/FixedButton.js
@@ -0,0 +1,9 @@
+import './FixedButton.css'
+function FixedButton(){
+    return (
+        <a href="/contact-us" className="fixedButton">
+            Contact Us
+        </a>
+    )
+}
+export default FixedButton;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Common/Header/Header";
+import Footer from "./Common/Footer/Footer";
+import FixedButton from "./Common/Contact/FixedButton";
+import FooterTop from "./Common/FooterTop/FooterTop";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <FixedButton />
+      <FooterTop/>
       <Footer />
     </>
   );
