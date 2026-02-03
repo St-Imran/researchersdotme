dfeced50e8cae5a3478681b41e4861a24c9da08a
Imran Khan <imran.khan5559@gmail.com>
2026-02-03 14:27:04 +0530

    removed unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import FixedButton from "../Common/Contact/FixedButton";
 import Hero from "./homepage/Hero";
-// import Home from "./homepage/Home";
 
 const LandingPage = () => {
   return (
     <>
       <FixedButton />
       <Hero />
-      {/* <Home /> */}
     </>
   );
 };
